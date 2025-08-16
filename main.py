807cb0c033f3639d1610e8fac0dc7e47e7bbfe4d
Caine Benoy <cainebenoy@gmail.com>
2025-08-16 19:38:00 +0530

    Added all quotes to quotes list

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import random
 from fastapi import FastAPI
 from typing import Dict, List
 app=FastAPI()
-QUOTES: List[Dict[str,str]]=[
-    {"text":"The only way to do great work is to love what you do.","author":"Steve Jobs"},
-    
+QUOTES: List[Dict[str, str]] = [
+    {"text": "The only way to do great work is to love what you do.", "author": "Steve Jobs"},
+    {"text": "Believe you can and you're halfway there.", "author": "Theodore Roosevelt"},
+    {"text": "The future belongs to those who believe in the beauty of their dreams.", "author": "Eleanor Roosevelt"},
+    {"text": "It is our choices, Harry, that show what we truly are, far more than our abilities.", "author": "J.K. Rowling"},
+    {"text": "The best way to predict the future is to create it.", "author": "Peter Drucker"},
+    {"text": "Start where you are. Use what you have. Do what you can.", "author": "Arthur Ashe"},
+    {"text": "Strive not to be a success, but rather to be of value.", "author": "Albert Einstein"}
+]
