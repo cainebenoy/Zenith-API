99b63508952835deff684601f5bfbd0a24e08867
Caine Benoy <cainebenoy@gmail.com>
2025-08-16 19:36:11 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import random
 from fastapi import FastAPI
 from typing import Dict, List
+app=FastAPI()
+QUOTES: List[Dict[str,str]]=[
+    {"text":"The only way to do great work is to love what you do.","author":"Steve Jobs"},
+    
